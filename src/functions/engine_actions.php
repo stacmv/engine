789b3e93c2bad96ae378eacb02aca91b7f0c3179
stacmv <stacmv@users.noreply.github.com>
2015-01-14 11:52:21 +0000

    * поддержка PHP > 5.5. (убран конфликт имен: password_*() переименованы в passwords_*() );

--- src/functions/engine_actions.php
+++ src/functions/engine_actions.php
@@ -380,7 +380,7 @@ function import_first_user_action(){
     if ( ! $login )  die("Code: ea-" . __LINE__);
     if ( ! $rights ) die("Code: ea-" . __LINE__);
     if ( ! $pass )   die("Code: ea-" . __LINE__);
-    $pass = password_hash($pass);
+    $pass = passwords_hash($pass);
     if ( ! $pass )   die("Code: ea-" . __LINE__);
     
 
