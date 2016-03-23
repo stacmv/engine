cb51c7dfaa9f63f40eadce877aaad549c9d40556
stacmv <stacmv@gmail.com>
2016-03-23 15:27:35 +0300

    * "short_open_tag" PHP setting is not required (and not checked) any more;

--- src/settings/checks.php
+++ src/settings/checks.php
@@ -5,9 +5,3 @@ if (PHP_VERSION_ID <= 50300){
     die("Code: engine-checks-".__LINE__."-PHP_VERSION_ID:".PHP_VERSION_ID);
 };
 
-// Short tags needed
-if ( ! ini_get("short_open_tag") ){
-    
-    die("Code: engine-checks-".__LINE__."-short_open_tag");
-    
-}
