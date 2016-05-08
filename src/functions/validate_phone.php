abe95554d7ed94ab9c3db047571ff9323b49c609
stacmv <stacmv@gmail.com>
2016-05-08 17:27:51 +0400

    * fixed backward compatibility issue with phone numbers validation: international phone validation extracted into separate validate_rule function, validation for local RU numbers restored.

--- src/functions/validate_phone.php
+++ src/functions/validate_phone.php
@@ -2,7 +2,7 @@
 function validate_rule_phone($key, $value, $rule_params, FormData $form){
     return validate_field_type_phone($key, $value, $form);
 }
-function validate_field_type_phone($key, $value, FormData $form){
+function validate_rule_phone_international($key, $value, FormData $form){
     
     $fields = form_get_fields($form->db_table, $form->form_name);
     $field = $fields[$key];
@@ -19,6 +19,26 @@ function validate_field_type_phone($key, $value, FormData $form){
             $res = false;
         }
         
+    }else{
+        $res = true;
+        
+    }
+        
+    return $res ? "" : _t("validate_phone_fail");
+};
+
+function validate_field_type_phone($key, $value, FormData $form){
+    
+    $fields = form_get_fields($form->db_table, $form->form_name);
+    $field = $fields[$key];
+    
+    
+    if ( ($field["required"] == "required") || $value ){
+    
+        $phone_cleared = glog_clear_phone($value); // номер телефона, только цифры.
+        
+        
+        $res = preg_match("/\d{10}$/",$phone_cleared);
     }else{
         $res = true;
         
