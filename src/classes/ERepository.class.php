446899007ac95d7c2c3f9a4f5745c194314a9944
stacmv <stacmv@gmail.com>
2016-06-20 00:56:22 +0400

    * ERepository: fixed bug in jsonSerialize() method;

--- src/classes/ERepository.class.php
+++ src/classes/ERepository.class.php
@@ -218,7 +218,7 @@ abstract class ERepository implements IteratorAggregate, jsonSerializable
             return $item->jsonSerialize();
         }, $this->fetchAll());
         
-        
+        return $res;
 
     }
     
