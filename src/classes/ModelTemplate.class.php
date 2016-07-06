e2307eb4879744585bca8b653b83b4d36ee5eb9c
stacmv <stacmv@gmail.com>
2016-07-06 12:04:49 +0400

    Added "lost" ModelTemplate and RepositoryTemplate classes

---
+++ src/classes/ModelTemplate.class.php
@@ -0,0 +1,22 @@
+<?php
+class ModelTemplate extends Model
+{
+    protected $repo_name;
+    protected $model_name;
+    
+    public function __construct(array $data = array()){
+        $this->model_name  = self::_get_model_name_for_class( get_called_class() );
+        $this->repo_name   = self::_get_repo_name_for_model_name($this->model_name);
+        
+        parent::__construct($data);
+    }
+    
+    
+    protected function _get_repo_name_for_model_name($model_name){
+        return _plural($model_name);
+    }
+    protected function _get_model_name_for_class($class_name){
+        return strtolower(implode(".", preg_split("/([[:upper:]][[:lower:]]+)/", $class_name, null, PREG_SPLIT_DELIM_CAPTURE|PREG_SPLIT_NO_EMPTY )));
+    }
+    
+}
---
+++ src/classes/RepositoryTemplate.class.php
@@ -0,0 +1,3 @@
+<?php
+class RepositoryTemplate extends Repository
+{}
