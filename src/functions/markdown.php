25865cd966fd31131ef794985d7017a0a16f38eb
stacmv <stacmv@gmail.com>
2017-02-01 08:51:55 +0300

    markdown: fixed bug when html entities are in source text;

--- src/functions/markdown.php
+++ src/functions/markdown.php
@@ -1,6 +1,8 @@
 <?php
 function markdown($markdown){
     $parsedown = new Parsedown();
-       
+    $markdown = html_entity_decode($markdown); 
+     
     return $parsedown->text($markdown);
+    
 }
