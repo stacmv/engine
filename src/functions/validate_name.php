36c443bbb7f1249cef34657c5a3a043e0e69170c
stacmv <stacmv@gmail.com>
2017-06-14 17:09:45 +0300

    * validate_name() & validate_phone() extracted from validate_field_type_name() & validate_field_type_phone() respectivly;

--- src/functions/validate_name.php
+++ src/functions/validate_name.php
@@ -5,22 +5,26 @@ function validate_rule_fio($key, $value, $rule_params, FormData $form){
     foreach($aFio as $name){
         $res = $res && (validate_field_type_name($key, $name, $form) == "");
     };
-    
+
     $res = $res && (count($aFio) >= 2 ); // Фамилия, Имя, Отчество ( в т.ч. без отчества)
-    
+
     return $res ? "" : _t("validate_fio_fail");
 };
 function validate_field_type_name($key, $value, FormData $form){
-    
+
     $fields = form_get_fields($form->db_table, $form->form_name);
     $field = $fields[$key];
-    
-    
+
+
     if ( ($field["required"] == "required") || $value ){
-        $res = preg_match("/^[АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя\-\s\.\"\(\)]+$/",$value);
+        $res = validate_name($value);
     }else{
         $res = true;
     }
-        
+
     return $res ? "" : sprintf(_t("validate_name_fail"), $field["label"], $field["label"]);
+}
+
+function validate_name($value){
+    return preg_match("/^[АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя\-\s\.\"\(\)]+$/",$value);
 }
--- src/functions/validate_phone.php
+++ src/functions/validate_phone.php
@@ -3,46 +3,47 @@ function validate_rule_phone($key, $value, $rule_params, FormData $form){
     return validate_field_type_phone($key, $value, $form);
 }
 function validate_rule_phone_international($key, $value, $rule_params, FormData $form){
-    
+
     $fields = form_get_fields($form->db_table, $form->form_name);
     $field = $fields[$key];
-    
-    
+
+
     if ( ($field["required"] == "required") || $value ){
-        
+
         $phoneUtil = \libphonenumber\PhoneNumberUtil::getInstance();
-        
+
         try {
             $phoneProto = $phoneUtil->parse($value, null);
             $res = $phoneUtil->isValidNumber($phoneProto);
         } catch (\libphonenumber\NumberParseException $e) {
             $res = false;
         }
-        
+
     }else{
         $res = true;
-        
+
     }
-        
+
     return $res ? "" : _t("validate_phone_fail");
 };
 
 function validate_field_type_phone($key, $value, FormData $form){
-    
+
     $fields = form_get_fields($form->db_table, $form->form_name);
     $field = $fields[$key];
-    
-    
+
+
     if ( ($field["required"] == "required") || $value ){
-    
-        $phone_cleared = glog_clear_phone($value); // номер телефона, только цифры.
-        
-        
-        $res = preg_match("/\d{10}$/",$phone_cleared);
+        $res = validate_phone($value);
     }else{
         $res = true;
-        
+
     }
-        
+
     return $res ? "" : _t("validate_phone_fail");
 };
+
+function validate_phone($value){
+    $phone_cleared = glog_clear_phone($value); // номер телефона, только цифры.
+    return preg_match("/\d{10}$/",$phone_cleared);
+}
