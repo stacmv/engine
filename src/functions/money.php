5a5de19190bd065a3ad498fd4453329022535fbc
stacmv <stacmv@gmail.com>
2017-09-14 18:13:47 +0300

    * added new argument 'html' for show_money() to set '&nbsp;' delimeter instead of space

--- src/functions/money.php
+++ src/functions/money.php
@@ -1,7 +1,7 @@
 <?php
-function show_money($value, $currency = "RUR", $lang = "ru"){
+function show_money($value, $html = true, $currency = "RUR", $lang = "ru"){
     if (is_numeric($value)){
-        return number_format($value, 2, ",", "&nbsp;");
+        return number_format($value, 2, ",", ($html ? "&nbsp;" : " ") );
     }else{
         return $value;
     }
