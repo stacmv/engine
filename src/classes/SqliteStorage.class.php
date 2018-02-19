8bccf0836968525789f0796d454d22cce8457a32
Stac <stacmv@gmail.com>
2018-02-19 13:34:36 +0300

    Removed deprecated in PHP7.2 each()

--- src/classes/SqliteStorage.class.php
+++ src/classes/SqliteStorage.class.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 class SqliteStorage extends EStorage
 {
     protected $dbh;
@@ -18,9 +18,9 @@ class SqliteStorage extends EStorage
     protected $sql_order_by;
     protected $sql_limit;
     protected $sql_offset;
-    
+
     protected $_sql_in_join; // JOIN method called, ON method not yet called
-   
+
     public function __construct($repo_name){
         $this->dbh = db_set($repo_name);
         $this->repo_name = $repo_name;
@@ -37,12 +37,12 @@ class SqliteStorage extends EStorage
     public function rollback(){
         return $this->dbh->rollbak();
     }
-    
+
     public function create($resource, ChangesSet $changes, $comment=""){
         // TODO error handling
         $db_table = $this->parseResource($resource)["path"];
         $res = db_add($db_table, $changes, $comment);
-        
+
         if (!$res){
             throw new Exception("fail");
         };
@@ -56,10 +56,10 @@ class SqliteStorage extends EStorage
         }else{
             return $this->fetchAllAssoc();
         };
-        
+
     }
     public function delete($resource, $comment = ""){
-        
+
         $id = $this->parseResource($resource)["id"];
         if ($id){
             $item = $this->read($resource);
@@ -70,7 +70,7 @@ class SqliteStorage extends EStorage
             return false;
         };
     }
-    
+
     public function fetch(){
         $row = $this->fetchAssoc();
         if (is_array($row) && !empty($row)){
@@ -85,38 +85,38 @@ class SqliteStorage extends EStorage
             $this->createSql();
             $this->sql_result = db_select($this->repo_name, $this->sql_query);
         };
-                
-        
+
+
         if ($this->sql_result){
-            list($k,$v) = each($this->sql_result);
+            $v = array_shift($this->sql_result);
             return $v;
         }else{
             return false;
         }
     }
     public function fetchAll(){
-        
+
         $this->fetchAllAssoc();
-        
+
         return array_map(function($row){
                 $modelClass = static::_getModelClassName($this->repo_name);
                 return new $modelClass($row);
             }, $this->sql_result);
-        
+
     }
     public function fetchAllAssoc(){
         if ( ! $this->sql_result){
             $this->createSql();
             $this->sql_result = db_select($this->repo_name, $this->sql_query);
         };
-        
+
         if ($this->sql_result){
             return $this->sql_result;
         }else{
             return false;
         }
     }
-    
+
     public function findOne($id){
         $res = $this->where("id = " . (int) $id)->fetchAll();
         return array_shift($res);
@@ -124,15 +124,15 @@ class SqliteStorage extends EStorage
     public function findWhere($whereClause){
         $repo_name = $this->repo_name;
         $sql = "SELECT * FROM " . db_get_table($repo_name) . " WHERE " . $whereClause . ";";
-        
+
         return array_filter(db_select($repo_name, $sql), function($item) use ($repo_name){
             return check_data_item_acl($item, $repo_name);
         });
     }
-    
+
     public function from($from){
         $this->sql_from = $from;
-        
+
         $this->sql_result = null;
         return $this;
     }
@@ -141,7 +141,7 @@ class SqliteStorage extends EStorage
 
         $this->sql_result = null;
         return $this;
-        
+
     }
     public function having($having){
         $this->sql_having = $having;
@@ -149,11 +149,11 @@ class SqliteStorage extends EStorage
         $this->sql_result = null;
         return $this;
     }
-    
+
     public function insert(EModel $model, $comment = ""){
         // TODO error handling
         $res = db_add($this->repo_name, $model->getChanges(), $comment);
-        
+
         if (!$res){
             throw new Exception("fail");
         };
@@ -163,7 +163,7 @@ class SqliteStorage extends EStorage
         if (!$this->_sql_in_join){
             $this->sql_join .= " INNER JOIN " . $join;
             $this->_sql_in_join = true;
-        
+
             $this->sql_result = null;
             return $this;
         }else{
@@ -174,7 +174,7 @@ class SqliteStorage extends EStorage
         if (!$this->_sql_in_join){
             $this->sql_join .= " LEFT JOIN " . $leftJoin;
             $this->_sql_in_join = true;
-        
+
             $this->sql_result = null;
             return $this;
         }else{
@@ -190,7 +190,7 @@ class SqliteStorage extends EStorage
         if ($this->_sql_in_join){
             $this->sql_join .= " ON " . $on . "";
             $this->_sql_in_join = false;
-        
+
             $this->sql_result = null;
             return $this;
         }else{
@@ -203,7 +203,7 @@ class SqliteStorage extends EStorage
         return $this;
     }
     public function orderBy(array $orderBy){
-        
+
         $this->sql_order_by = array();
         foreach($orderBy as $field=>$sort_mode){
             if (in_array(strtoupper($sort_mode), array("ASC", "DESC"))){
@@ -212,7 +212,7 @@ class SqliteStorage extends EStorage
         }
         $this->sql_result = null;
         return $this;
-        
+
     }
     public function select($select){
         $fields  = $this->fields;
@@ -222,19 +222,19 @@ class SqliteStorage extends EStorage
             if (is_string($select)){
                 $select = array($select);
             };
-            
+
             $this->sql_select = $select;
         }
-        
+
         $this->sql_result = null;
-        
+
         return $this;
     }
     public function set(array $set){
-        
+
         if (!$this->sql_where)  throw new Exception("wrong_call_chain_".__METHOD__);
-        
-                
+
+
         foreach ($set as $field=>$value){
             if (isset($this->fields[$field])){
                 $set[$field] = db_prepare_value($value, $this->fields[$field]["type"]);
@@ -242,8 +242,8 @@ class SqliteStorage extends EStorage
                 throw new Exception("wrong_param_".(string)$field);
             };
         };
-        
-        
+
+
         $sql = "UPDATE ";
         //
         if (!$this->sql_from){
@@ -251,23 +251,23 @@ class SqliteStorage extends EStorage
         }else{
             $sql .= $this->sql_from;
         };
-        
-        
+
+
         // SET
         $sql .= " SET " . implode(", ", array_map(function($value, $field){
             return $field . " = " . (is_null($value) ? "NULL" : db_quote($value));
         }, $set, array_keys($set)));
-        
+
         // Where
         if ( ! userHasRight("manager") && isset($this->fields["user_id"])){
             $where_acl_addon = " (user_id = " . (int) $_USER["id"] . ") ";
-        }        
+        }
         if ($this->sql_where || !empty($where_acl_addon))  $sql .= " WHERE ";
         if ($this->sql_where)                              $sql .= $this->sql_where;
         if ($this->sql_where && !empty($where_acl_addon))  $sql .= " AND ";
         if (!empty($where_acl_addon))                      $sql .= $where_acl_addon;
-        
-        
+
+
         $st = $this->dbh->prepare($sql);
         if ($st){
             if ($st->execute()){
@@ -279,16 +279,16 @@ class SqliteStorage extends EStorage
             dosyslog(__METHOD__.get_callee().": NOTICE: Wrong SQL: ".$sql.". Error: ".db_error($this->dbh));
             throw new Exception("wrong_sql_query");
         }
-        
+
         return $st->rowCount();
-        
+
     }
     public function update($resource, ChangesSet $changes, $comment=""){
         // TODO error handling
-        
+
         $db_table = $this->parseResource($resource)["path"];
         $id       = $this->parseResource($resource)["id"];
-        
+
         if ($db_table && $id){
             list($res, $reason) = db_edit($db_table, $id, $changes, $comment);
             if (!$res){
@@ -304,40 +304,40 @@ class SqliteStorage extends EStorage
         $this->sql_result = null;
         return $this;
     }
-    
+
     public function reset(){
         // reset all sql attributes
         $properties = get_class_vars(__CLASS__);
-        
+
         foreach($properties as $k=>$v){
             if (substr($k, 0, 4) == "sql_"){
                 $this->$k = null;
             };
         };
-        
+
     }
-    
+
     protected function createSql(){
         global $_USER;
-        
-        
+
+
         $this->sql_hash = md5(implode("::", array_map("serialize", array(
             $this->sql_select, $this->sql_where, $this->sql_order_by, $this->sql_limit
         ))));
-        
+
         $sql = "SELECT ";
         if ($this->sql_select){
             $sql .= implode(", ", $this->sql_select);
         }else{
             $sql .= "*";
         }
-        
+
         if (!$this->sql_from){
             $sql .= " FROM " . $this->db_table ." ";
         }else{
             $sql .= " FROM " . $this->sql_from;
         };
-        
+
         // Join
         if ($this->sql_join){
             if (!$this->_sql_in_join){
@@ -346,17 +346,17 @@ class SqliteStorage extends EStorage
                 throw new Exception("wrong_call_chain_".__METHOD__);
             }
         }
-        
+
         // Where
         if ( ! userHasRight("manager") && isset($this->fields["user_id"])){
             $where_acl_addon = " (user_id = " . (int) $_USER["id"] . ") ";
-        }        
+        }
         $sql_where = "";
         if ($this->sql_where || !empty($where_acl_addon))  $sql_where .= " WHERE ";
         if ($this->sql_where)                              $sql_where .= $this->sql_where;
         if ($this->sql_where && !empty($where_acl_addon))  $sql_where .= " AND ";
         if (!empty($where_acl_addon))                      $sql_where .= $where_acl_addon;
-        
+
         // Do not return 'marked as deleted' records by default
         if (isset($this->fields["deleted"])){
             if (strpos($sql_where, "deleted") === false){
@@ -364,43 +364,43 @@ class SqliteStorage extends EStorage
                 else $sql_where .= " AND ".$this->db_table.".deleted IS NULL";
             };
         };
-        
+
         $sql .= $sql_where;
-        
+
         // dump($sql_where,"sql_where");
         // die($sql);
-            
+
         // group by
         if ($this->sql_group_by){
             $sql .= " GROUP BY " . $this->sql_group_by . " ";
         }
-        
+
         // Having
         if ($this->sql_having){
             $sql .= " HAVING " . $this->sql_having . " ";
         }
-        
+
         // Order by
         if ($this->sql_order_by){
             $sql .= " ORDER BY " . implode(", ", $this->sql_order_by). " ";
         } elseif (!$this->sql_join) {
             $sql .= " ORDER BY created DESC ";
         }
-        
+
         // Limit
         if ($this->sql_limit){
             $sql .= " LIMIT ".$this->sql_limit;
         }
-        
+
         // Offset
         if ($this->sql_offset){
             $sql .= " OFFSET ".$this->sql_offset;
         }
         $sql .= ";";
-        
+
         $this->sql_query = $sql;
         $this->sql_result = null;
-        
+
         return $sql;
     }
 
